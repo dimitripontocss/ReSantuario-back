e4526d7cc088e3111d54d49411b84afe3cddc87d
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-29 14:46:25 -0300

    feat:  verify if username is beeing used

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,33 +1,42 @@
 import { prisma } from "../databaseStrategy/database";
-import { TUser } from "../interfaces/interfaces"
+import { TUser } from "../interfaces/interfaces";
 
 export async function insertUser(userData: TUser) {
-    await prisma.user.create({
-        data: userData
-    })
+  await prisma.user.create({
+    data: userData,
+  });
 }
 
 export async function findSingleUser(email: string) {
-    const result = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-    return result;
+  const result = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return result;
+}
+
+export async function findSingleUserByUsername(userName: string) {
+  const result = await prisma.user.findUnique({
+    where: {
+      userName,
+    },
+  });
+  return result;
 }
 
 export async function findUserById(id: number) {
-    const result = await prisma.user.findUnique({
-        where:{id}
-    })
-    return result;
+  const result = await prisma.user.findUnique({
+    where: { id },
+  });
+  return result;
 }
 
 export async function getAllUsersEmails() {
-    const result = await prisma.user.findMany({
-        select:{
-            email:true
-        }
-    })
-    return result;
+  const result = await prisma.user.findMany({
+    select: {
+      email: true,
+    },
+  });
+  return result;
 }
