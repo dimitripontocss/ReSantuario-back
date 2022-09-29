fe4eed372078ec6ba768226b45ae3553daa32693
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-29 18:30:33 -0300

    fix: jwtAuthentication optimized

--- src/middlewares/jwtAuthMiddleware.ts
+++ src/middlewares/jwtAuthMiddleware.ts
@@ -3,13 +3,12 @@ import { Request, Response, NextFunction } from "express";
 
 import * as userService from "../services/userService";
 
-export async function jtwAuth(req: Request, res: Response, next:NextFunction){
-    const authorization = req.headers['authorization'];
-  if (!authorization) throw {name: "not_found", message: "Missing authorization header"};
-
-  const token = authorization.replace('Bearer ', '');
-  if (!token) throw {name: "not_found", message: "Missing token"};
-
+export async function jtwAuth(req: Request, res: Response, next: NextFunction) {
+  const authorization = req.headers["authorization"];
+  if (!authorization)
+    throw { name: "not_found", message: "Missing authorization header" };
+  const token = authorization.replace("Bearer ", "");
+  if (!token) throw { name: "not_found", message: "Missing token" };
   try {
     const JWT_KEY = process.env.JWT_KEY;
     const { userId } = jwt.verify(token, JWT_KEY) as { userId: number };
@@ -17,6 +16,6 @@ export async function jtwAuth(req: Request, res: Response, next:NextFunction){
     res.locals.user = user;
     next();
   } catch {
-    throw {name: "auth_error", message: "Invalid token"};
+    throw { name: "auth_error", message: "Invalid token" };
   }
 }
