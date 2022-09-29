8f85e645dcfe202725c8791631948ced033672bb
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-29 14:33:47 -0300

    feat: new interfaces created

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,11 +1,29 @@
-import { User } from "@prisma/client";
+import { User, Ingredient, Recipe, NutritionalTable } from "@prisma/client";
+
 export type TUser = Omit<User, "id" | "profilePicture">;
+export type TIngredient = Omit<Ingredient, "id">;
+export type TRecipe = Omit<Recipe, "id">;
+export type TNutritionalTable = Omit<NutritionalTable, "id">;
 
 export interface INewUserData {
   userName: string;
   email: string;
   password: string;
   passwordConfirmation: string;
+}
+
+export interface IIngredient {
+  name: string;
+  amount: string;
+}
+
+export interface INewRecipeData {
+  title: string;
+  pictureUrl: string;
+  instructions: string;
+  category: string;
+  difficulty: number;
+  ingredients: IIngredient[];
 }
 
 export interface IError {
